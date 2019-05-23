eb8bd07388500e74e23cf27f870d0e72af7ec120
pawcio <pawel860@gmail.com>
2019-05-23 20:52:29 +0200

    created post and read requests

--- server/routes/api/feedings.js
+++ server/routes/api/feedings.js
@@ -1,34 +1,78 @@
 const express = require("express");
 const mongobd = require("mongodb");
-
-const fakeData = require("../../data/fakeData");
+const DbUrl = "mongodb+srv://login:passs@cluster.mongodb.net";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send(fakeData);
+router.get("/", async (req, res) => {
+  const MongoClient = mongobd.MongoClient;
+  const client = new MongoClient(DbUrl, { useNewUrlParser: true });
+
+  await client.connect(async err => {
+    const dates = client.db("boboapp").collection("dates");
+
+    const ff = await dates
+      .aggregate([
+        {
+          $lookup: {
+            from: "feedings",
+            localField: "_id",
+            foreignField: "dateId",
+            as: "feedings"
+          }
+        }
+      ])
+      .toArray();
+
+    client.close();
+    res.send(ff);
+  });
+
+  // try {
+  //   const data = await loadDataFromDB();
+  //   res.status(200).send(await data.find({}).toArray());
+  // } catch (err) {
+  //   console.log(err);
+  // }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const payload = req.body;
+  const MongoClient = mongobd.MongoClient;
+  const client = new MongoClient(DbUrl, { useNewUrlParser: true });
 
-  if (payload) {
-    fakeData.push(payload);
-    res.status(201).send();
+  const date = payload.date;
+  const feeding = {
+    hour: payload.hour,
+    description: payload.description,
+    amount: payload.amount
+  };
+
+  if (date && feeding) {
+    await client.connect(async err => {
+      if (err) {
+        console.log(err);
+        return res.status(400).send();
+      }
+
+      const dates = client.db("boboapp").collection("dates");
+      const feedings = client.db("boboapp").collection("feedings");
+      let today = await dates.findOne({ date: date });
+
+      if (!today) {
+        today = {
+          date
+        };
+        await dates.insertOne(today);
+      }
+
+      feeding.dateId = today._id;
+      await feedings.insertOne(feeding);
+
+      client.close();
+      return res.status(201).send();
+    });
   }
-
-  res.status(400).send();
 });
-
-async function loadDataFromDB() {
-  const client = await mongobd.MongoClient.connect(
-    "mongodb://CONNECTION_STRING",
-    {
-      useNewUrlParser: true
-    }
-  );
-
-  return client.db("boboEats").collection("feedings");
-}
 
 module.exports = router;
